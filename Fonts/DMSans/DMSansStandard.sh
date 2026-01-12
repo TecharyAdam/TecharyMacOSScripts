909b1b7f5ddf49ea7b06f4ae6ece146d88a29616
TecharyAdam <86409453+TecharyAdam@users.noreply.github.com>
2026-01-12 10:08:06 +0000

    Update font script to handle DMSansStandard font

--- Fonts/DMSans/DMSansStandard.sh
+++ Fonts/DMSans/DMSansStandard.sh
@@ -4,18 +4,18 @@
 font_url='https://raw.githubusercontent.com/TecharyAdam/TecharyMacOSScripts/refs/heads/main/Fonts/DMSans/DMSans-VariableFont_opsz%2Cwght.ttf'
 
 # Check if the font file already exists in /Library/Fonts
-if [ -f "/Library/Fonts/JulieRegular.ttf" ]; then
+if [ -f "/Library/Fonts/DMSansStandard.ttf" ]; then
   echo "Font already exists in /Library/Fonts. No need to download."
 else
   # Download the font to a temporary location
-  tmp_file="/tmp/JulieRegular.TTF"
+  tmp_file="/tmp/DMSansStandard.TTF"
   curl -o "$tmp_file" "$font_url"
 
   # Check if the download was successful
   if [ $? -eq 0 ]; then
     # Use sudo to copy the font to /Library/Fonts
     sudo cp "$tmp_file" "/Library/Fonts/"
-    sudo chown root:wheel "/Library/Fonts/JulieRegular.TTF"
+    sudo chown root:wheel "/Library/Fonts/DMSansStandard.TTF"
     echo "Font copied to /Library/Fonts successfully."
   else
     echo "Failed to download the font."
