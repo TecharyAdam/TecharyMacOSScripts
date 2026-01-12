58852835ee192ead2bde6bbd17290fb9dc71b3b4
TecharyAdam <86409453+TecharyAdam@users.noreply.github.com>
2026-01-12 10:22:01 +0000

    Add script to download DMSerifItalic font

---
+++ Fonts/DMSerif/DMSerifItalic.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+# Font download URL
+font_url='https://raw.githubusercontent.com/TecharyAdam/TecharyMacOSScripts/refs/heads/main/Fonts/DMSerif/DMSerifStandard.ttf'
+
+# Check if the font file already exists in /Library/Fonts
+if [ -f "/Library/Fonts/DMSerifItalic.ttf" ]; then
+  echo "Font already exists in /Library/Fonts. No need to download."
+else
+  # Download the font to a temporary location
+  tmp_file="/tmp/DMSerifItalic.TTF"
+  curl -o "$tmp_file" "$font_url"
+
+  # Check if the download was successful
+  if [ $? -eq 0 ]; then
+    # Use sudo to copy the font to /Library/Fonts
+    sudo cp "$tmp_file" "/Library/Fonts/"
+    sudo chown root:wheel "/Library/Fonts/DMSerifItalic.TTF"
+    echo "Font copied to /Library/Fonts successfully."
+  else
+    echo "Failed to download the font."
+  fi
+
+  # Clean up the temporary file
+  rm -f "$tmp_file"
+fi
